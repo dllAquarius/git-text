e71d66cf2241bd621a45948ff53280ca989bc245
dllAquarius <dllAquarius@163.com>
2021-11-28 11:49:05 +0800

    first commit

---
+++ src/main/java/com/aquarius/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.aquarius.git;
+
+/**
+ * @Auther: dllAquarius
+ * @Date: 2021/11/28
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
