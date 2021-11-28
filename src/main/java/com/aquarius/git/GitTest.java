7fea5509c403190ccef06b2c1c73ef336ae81305
dllAquarius <dllAquarius@163.com>
2021-11-28 11:50:59 +0800

    secend commit

--- src/main/java/com/aquarius/git/GitTest.java
+++ src/main/java/com/aquarius/git/GitTest.java
@@ -8,5 +8,6 @@ package com.aquarius.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
     }
 }
